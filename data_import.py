241f41bdb4a272d0fd70bbeb8b39df6558d33c1d
George B <gbergmark4@gmail.com>
2014-08-13 23:04:35 -0400

    added a writer module, still rough. no case for if file is new. no current capability to check for redundant entries

--- data_import.py
+++ data_import.py
@@ -6,10 +6,17 @@
 
 import csv
 
+COMMONFILE = 'new.csv'
+
 def data_open(csvfile):
     '''csv file -> list of dictionary entries'''
     with open(csvfile) as new_file:
         new_entries = [each for each in csv.DictReader(new_file)]
     return new_entries
 
-
+def combine_data(csvDictReaderList):
+    COMMONFILE = 'new.csv'
+    '''write new entries to csv file. *do not write header info or redundant entries*'''
+    with open(COMMONFILE,'a') as main_list:
+        writer = csv.DictWriter(COMMONFILE,[each for each in csvDictReaderList[0])
+        writer.writerows(csvDictReaderList)
