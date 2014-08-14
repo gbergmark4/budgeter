df54c58ec90b27277f20bca128cf73a32a8d303b
gbergmark4 <gbergmark4@gmail.com>
2014-08-14 17:05:54 -0400

    data writer now complete. data comparison in progress

--- data_import.py
+++ data_import.py
@@ -5,7 +5,6 @@
 ###takes in csv data and combines it with a common file, ensures there's no redundant entry.
 
 import csv
-
 COMMONFILE = 'new.csv'
 
 def data_open(csvfile):
@@ -14,9 +13,22 @@ def data_open(csvfile):
         new_entries = [each for each in csv.DictReader(new_file)]
     return new_entries
 
-def combine_data(csvDictReaderList):
-    COMMONFILE = 'new.csv'
-    '''write new entries to csv file. *do not write header info or redundant entries*'''
-    with open(COMMONFILE,'a') as main_list:
-        writer = csv.DictWriter(COMMONFILE,[each for each in csvDictReaderList[0])
+def combine_data(new_entries):
+	'''the tricky one. combine old data and new data 
+	into one big file. remove any redundant entries'''
+	with open(COMMONFILE,'rb') as old_list:
+		old_entries = [each for each in csv.DictReader(old_list)]
+	for new in new_entries:
+		for old in old_entries:
+			if new == old:
+				new_entries.pop(new_entries.index(new))
+	return new_entries + old_entries
+
+
+
+def write_data(csvDictReaderList):
+    '''write all entries to csv file. Redundant entries should be removed by now'''
+    with open(COMMONFILE,'wb') as main_list:
+        writer = csv.DictWriter(main_list, [each for each in csvDictReaderList[0]])
+        writer.writeheader()
         writer.writerows(csvDictReaderList)
